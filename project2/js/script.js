dbae20cfa6198ee064c0abf2945d997fe69918b1
Alex <suslyn_alex@mail.ru>
2021-09-15 23:36:04 +0300

    add 14 task

--- project2/js/script.js
+++ project2/js/script.js
@@ -92,7 +92,7 @@ window.addEventListener('DOMContentLoaded', function () {
         document.body.style.overflow = "";
     });
 
-    //Отпрвкка форм
+    //Отправка форм
 
     let message = {
         loading: 'Загрузка..',
@@ -100,40 +100,65 @@ window.addEventListener('DOMContentLoaded', function () {
         failure: 'Что-то пошло не так',
     };
     let modalForm = document.querySelector('.main-form'),
-        contactForm = document.querySelector('#form'),
+        bottomForm = document.getElementById('form'),
         input = document.querySelectorAll('input'),
         statusMessage = document.createElement('div');
 
-    statusMessage.classList.add('status');
 
-    let requestFunc = function (e) {
-        e.preventDefault();
-        this.appendChild(statusMessage);
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-        let formData = new FormData(this);
-        let obj = {};
-        formData.forEach(function (item, index) {
-            obj[index] = item;
-        })
-        let json = JSON.stringify(obj);
-        request.send(json);
 
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.textContent = message.loading;
-            } else if (request.readyState == 4 && request.status == 200) {
-                statusMessage.textContent = message.success;
-            } else {
-                statusMessage.textContent = message.failure;
-            }
-        })
+    function sendForm(elem) {
+        elem.addEventListener('submit', function (e) {
+            e.preventDefault();
+            statusMessage.classList.add('status');
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+            let obj = {};
+            let json;
+            formData.forEach(function (item, index) {
+                obj[index] = item;
+                json = JSON.stringify(obj);
+            });
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = "";
-        }
-    };
-    modalForm.addEventListener('submit', requestFunc);
-    contactForm.addEventListener('submit', requestFunc);
+            function requestFunc(data) {
+                return new Promise((resolve, reject) => {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php', true);
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                    console.log(data)
+
+                    request.addEventListener('readystatechange', function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState == 4) {
+                            if (request.status == 200) {
+                                resolve();
+                            } else {
+                                reject();
+                            }
+                        }
+                    });
+                    request.send(data);
+                });
+            };
+            requestFunc(json)
+                .then(() => {
+                    statusMessage.textContent = message.loading;
+                })
+                .then(() => {
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusMessage.textContent = message.failure;
+                })
+                .then(() => {
+                    for (let i = 0; i < input.length; i++) {
+                        input[i].value = "";
+                    }
+                })
+        });
+    }
+
+    sendForm(modalForm);
+    sendForm(bottomForm);
+
 });
